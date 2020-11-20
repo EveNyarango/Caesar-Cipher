9f83218588cc896bbcd1929ed23f2dff35484049
EveNyarango <evenyarango@gmail.com>
2020-11-20 22:57:17 +0300

    generate constructor and getter for encode class

--- src/main/java/models/Encode.java
+++ src/main/java/models/Encode.java
@@ -2,4 +2,21 @@ package models;
 
 public class Encode {
 
+    private  String mInputString;
+    private int mShift;
+
+//    constructor
+
+    public Encode(String mInputString, int mShift) {
+        this.mInputString = mInputString;
+        this.mShift = mShift;
+    }
+
+    public String getmInputString() {
+        return mInputString;
+    }
+
+    public int getmShift() {
+        return mShift;
+    }
 }
