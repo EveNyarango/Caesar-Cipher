4194ee14c6f5f9a075f6f7331a1f6e8ed5688c9a
EveNyarango <evenyarango@gmail.com>
2020-11-21 01:15:29 +0300

    add the front-end part that prints out the results and gives user a chance to input data

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,3 +1,7 @@
+import com.sun.tools.javac.comp.Enter;
+import models.Decode;
+import models.Encode;
+
 import java.util.Scanner;
 
 public class App {
@@ -6,12 +10,46 @@ Scanner myScanner = new Scanner (System.in);
 boolean runCaesar = true;
 
 while (runCaesar) {
+
 System.out.println("Hi, this Caesar Cipher");
 System.out.println("=======================");
 System.out.println("Enter encode to encrypt or decode to decrypt or exit to leave the group");
 String choice = myScanner.nextLine();
 
+if (choice.equals("encode")){
+    System.out.println("models.Encode the message by entering it in the next line");
+    String input = myScanner.nextLine();
+    System.out.println("====================");
 
+    System.out.println("Enter a key");
+    int key = myScanner.nextInt();
+    System.out.println("===================");
+    Encode caesar = new Encode(input, key);
+    String encoded = Encode.encode(caesar);
+    System.out.println("Encoded message:" + encoded);
+    System.out.println("=========================");
+    System.out.println("=========================");
+    System.out.println("=========================");
+}
+else if(choice.equals("decode")){
+    System.out.println("To decode a message enter it below");
+    String mess = myScanner.nextLine();
+    System.out.println("==============================");
+
+    System.out.println("Enter the key you used");
+    int nom = myScanner.nextInt();
+    System.out.println("==========================");
+    Decode caesar = new Decode(mess, nom);
+    String decoded = Decode.decode(caesar);
+
+    System.out.println("Decoded message:" + decoded);
+    System.out.println("=========================");
+    System.out.println("=========================");
+    System.out.println("=========================");
+}
+else if (choice.equals("exit")){
+    runCaesar = false;
+}
 }
   }
 }
