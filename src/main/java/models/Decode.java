4fd51eb9d414615278196ceb504ba1ff339f21e9
EveNyarango <evenyarango@gmail.com>
2020-11-20 22:06:39 +0300

    add for loop method on the output string

--- src/main/java/models/Decode.java
+++ src/main/java/models/Decode.java
@@ -17,6 +17,48 @@ public class Decode {
         return mShift;
     }
     public static  String decode(Decode decode){
+if(decode.mShift >26) {
+decode.mShift = decode.mShift % 26;
+}else if(decode.mShift < 0){
+    decode.mShift = (decode.mShift % 26) + 26;
+
+}
+String outputString = "";
+int length = decode.mInputString.length();
+for (int i = 0; i < length; i++){
+    char cc = decode.mInputString.charAt(i);
+    if (Character.isLetter(cc)){
+        if (Character.isLowerCase(cc)){
+            char c = (char)(cc + (26-decode.mShift));
+            if (c>'z'){
+                outputString += (char)(cc + (26-decode.mShift));
+            }
+
+                else {
+                    outputString += c;
+
+            }
+        }
+
+       else if (Character.isUpperCase(cc)){
+            char c = (char)(cc + (26-decode.mShift));
+            if (c>'Z'){
+                outputString += (char)(cc + (26-decode.mShift));
+            }
+
+            else {
+                outputString += c;
+
+            }
+        }
+
+
+    }
+    else {
+        outputString += cc;
+    }
+}
+return outputString;
 
     }
 }
