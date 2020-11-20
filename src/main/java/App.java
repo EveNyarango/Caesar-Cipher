3735c7d22859261f211da126f1fb1402e636c79e
EveNyarango <evenyarango@gmail.com>
2020-11-21 00:33:05 +0300

    insert and import scanner user input

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,17 @@
+import java.util.Scanner;
+
 public class App {
+  public static void main (String[] args){
+Scanner myScanner = new Scanner (System.in);
+boolean runCaesar = true;
+
+while (runCaesar) {
+System.out.println("Hi, this Caesar Cipher");
+System.out.println("=======================");
+System.out.println("Enter encode to encrypt or decode to decrypt or exit to leave the group");
+String choice = myScanner.nextLine();
+
+
+}
+  }
 }
