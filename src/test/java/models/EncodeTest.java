0a14a7c39ed66c9e215edbb1601fab36187219b7
EveNyarango <evenyarango@gmail.com>
2020-11-21 02:23:46 +0300

    run test on decoding and encoding

--- src/test/java/models/EncodeTest.java
+++ src/test/java/models/EncodeTest.java
@@ -7,12 +7,12 @@ import static org.junit.Assert.*;
 public class EncodeTest {
     @Test
     public void runEncode_ifInputIsString() {
-Encode testEncode = new Encode("abc",3);
-assertEquals("abc", testEncode.getmInputString());
+Encode testEncode = new Encode("xyz",3);
+assertEquals("xyz", testEncode.getmInputString());
     }
     @Test
     public void  runDecoding_ifKeyIsInt() {
-        Encode testEncode = new Encode("abc", 3);
+        Encode testEncode = new Encode("xyz", 3);
         assertEquals(3, testEncode.getmShift());
     }
     @Test
