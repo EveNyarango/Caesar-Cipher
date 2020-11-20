f884c711c9df414409eca504ffddd1af44630d1a
EveNyarango <evenyarango@gmail.com>
2020-11-20 23:23:43 +0300

    loop the characters with the output string

--- src/main/java/models/Encode.java
+++ src/main/java/models/Encode.java
@@ -27,5 +27,34 @@ public class Encode {
     encode.mShift = (encode.mShift % 26) + 26;
         }
 
+        String outputString = "";
+        int length = encode.mInputString.length();
+        for (int i = 0; i < length ; i++) {
+            char cc = encode.mInputString.charAt(i);
+            if(Character.isLetter(cc)){
+                if(Character.isLowerCase(cc)){
+                    char c = (char)(cc-encode.mShift);
+                    if(c<'a'){
+                        outputString += (char)(cc + (26-encode.mShift));
+                    }
+                    else {
+                        outputString += c;
+                    }
+                }
+                else if(Character.isUpperCase(cc)){
+                    char c = (char)(cc-encode.mShift);
+                    if(c<'A'){
+                        outputString += (char)(cc + (26-encode.mShift));
+                    }
+                    else {
+                        outputString += c;
+                    }
+                }
+            }
+            else{
+                outputString += cc;
+            }
+        }
+        return outputString;
     }
 }
